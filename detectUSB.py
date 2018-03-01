867d6a6c5c7c04a24b365c2a4421c37b3b9b9e28
quanght55 <htq0505@gmail.com>
2018-03-01 16:27:57 +0700

    Test

--- detectUSB.py
+++ detectUSB.py
@@ -26,7 +26,7 @@ def get_info(devices):
 		my_output_list = stdOutValue.split("\n")
 
 		for word in my_output_list:
-			print word.find("Mass Storage")
+			print word
 	#for mass in mass_storage:
 		#subprocess.Popen(["lsusb","-D", item],stdout=subprocess.PIPE, stderr=subprocess.PIPE) 
 get_info(get_usb())
