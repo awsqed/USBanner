3f53b613f5cdab1eef876e0e9901d8ad5754aafa
quanght55 <htq0505@gmail.com>
2018-03-04 18:09:43 +0700

    edit detectUSB.py

--- detectUSB.py
+++ detectUSB.py
@@ -1,7 +1,7 @@
 import re, sys, os
 import subprocess
 import pyudev
-
+import usb.core, usb.util
 
 def event_handle():
 	context = pyudev.Context()
@@ -9,33 +9,20 @@ def event_handle():
 	monitor.filter_by(subsystem='usb')
 	for device in iter(monitor.poll, None):
 		if device.action == 'add':
-			with open("test/logs/add.log","a+") as f:
-				if None != device.get('ID_VENDOR_ID'):
-					f.write('Vendor: {0} , Model: {1} connected\n'.format(device.get('ID_VENDOR_ID'), device.get('ID_MODEL_ID')))
+			# idVendor and idProduct will be hex
+			if None != device.get('ID_VENDOR_ID'):
+				print type(device.get('ID_VENDOR_ID'))
+				dev = usb.core.find(idVendor=device.get('ID_VENDOR_ID'), idProduct=device.get('ID_MODEL_ID'))
+				for cfg in dev:
+					for i in cfg:
+						print i.bInterfaceClass # Get interface class , Mass storage will equal to 8
+				with open("test/logs/add.log","a+") as f:
+					f.write('VendorID: {0} , ModelID: {1} connected\n'.format(device.get('ID_VENDOR_ID'), device.get('ID_MODEL_ID')))
 		if device.action == 'remove':
 			with open("test/logs/remove.log","a+") as f:
 				if None != device.get('ID_VENDOR_ID'):
-					f.write('Vendor: {0} , Model: {1} connected\n'.format(device.get('ID_VENDOR_ID'), device.get('ID_MODEL_ID')))	
-
-def get_usb():
-	device_re = re.compile("Bus\s+(?P<bus>\d+)\s+Device\s+(?P<device>\d+).+ID\s(?P<id>\w+:\w+)\s(?P<tag>.+)$", re.I)
-	df = subprocess.check_output("lsusb")
-	devices = []
-	for i in df.split('\n'):
-		if i:
-			info = device_re.match(i)
-			if info:
-				dinfo = info.groupdict()
-				dinfo['device'] = '/dev/bus/usb/%s/%s' % (dinfo.pop('bus'), dinfo.pop('device'))
-				devices.append(dinfo)
-	for device in devices:
-		print device
-	#for device in devices:
-	#	temp = device.get("device")
-	#	print temp
-	#	command = ['lsusb', '-D', '{}'.format(temp), "|", "grep", "-Ei", "'(idVendor|Mass Storage)'"]
-	#	print subprocess.check_output(command,shell=True) 
-
+					f.write('VendorID: {0} , ModelID: {1} disconnected\n'.format(device.get('ID_VENDOR_ID'), device.get('ID_MODEL_ID')))
+		
 event_handle()
 
 
