b74121620f6c1c30758b1fbcd73b3f03a1ae332a
quanght55 <htq0505@gmail.com>
2018-03-06 00:10:13 +0700

    add logs creator function

--- detectUSB.py
+++ detectUSB.py
@@ -1,10 +1,13 @@
-import re, sys, os, datetime
+import re, sys, os, datetime, time
 import subprocess
 import pyudev
 import usb.core, usb.util
 
+LOG_FOLDER='/home/quanghuynh/USBanner/test/logs/{}'.format(time.strftime("%Y-%m-%d"))
+LOG_FORMAT='[0] [{0}] [{1}]: {2}'
 
 def event_handle():
+	logs_folder_creator()
 	context = pyudev.Context()
 	monitor = pyudev.Monitor.from_netlink(context)
 	monitor.filter_by(subsystem='usb')
@@ -16,19 +19,22 @@ def event_handle():
 				for cfg in dev:
 					for i in cfg:
 						if i.bInterfaceClass == 8: # Get interface class , Mass storage will equal to 8
-							with open("test/logs/add.log","a+") as f:
-								f.write('{0} | INFO | VendorID: {1} | ModelID: {2} | connected | Mass storage: Yes.\n'.format(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),device.get('ID_VENDOR_ID'), device.get('ID_MODEL_ID')))
-								f.close()
-						else:
-							with open("test/logs/add.log","a+") as f:
-								f.write('{0} | INFO | VendorID: {1} | ModelID: {2} | connected | Mass storage: No.\n'.format(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),device.get('ID_VENDOR_ID'), device.get('ID_MODEL_ID')))
+							with open(LOG_FOLDER+"/add.log","a+") as f:
+								temp = LOG_FORMAT.format(datetime.datetime.now().strftime("%H:%M:%S"),'INFO',device.get('ID_VENDOR_ID')+':'+device.get('ID_MODEL_ID')) +" is plugged in.\n"
+								f.write(temp)
 								f.close()
 		if device.action == 'remove':
-			with open("test/logs/remove.log","a+") as f:
+			with open(LOG_FOLDER+"/remove.log","a+") as f:
 				if None != device.get('ID_VENDOR_ID'):
-					f.write('{0} | INFO | VendorID: {1} | ModelID: {2} | disconnected.\n'.format(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),device.get('ID_VENDOR_ID'), device.get('ID_MODEL_ID')))
+					temp = LOG_FORMAT.format(datetime.datetime.now().strftime("%H:%M:%S"),'INFO',device.get('ID_VENDOR_ID')+':'+device.get('ID_MODEL_ID')) +" is unplugged.\n"
+					f.write(temp)
 					f.close()
-		
+
+def logs_folder_creator():
+	if os.path.exists("/home/quanghuynh/USBanner/test/logs/{}".format(time.strftime("%Y-%m-%d"))) == False:
+		os.makedirs("/home/quanghuynh/USBanner/test/logs/{}".format(time.strftime("%Y-%m-%d")))
+
+
 event_handle()
 
 
