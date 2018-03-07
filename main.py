99aba80d343c18076f8ba325beb84a8be2290b55
awsqed <8575867+awsqed@users.noreply.github.com>
2018-03-08 00:37:19 +0700

    Use another thread to handle device monitor

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import re, sys, os, datetime, time, getpass
+import re, sys, os, datetime, time, getpass, threading
 import subprocess
 import pyudev
 import usb.core, usb.util
@@ -9,6 +9,7 @@ USB_LIST = []
 def create_log_folder(name):
     path = "logs/" + name
     if os.path.exists(path) == False:
+        print "Log folder doesn't exist, creating one..."
         os.makedirs(path)
 
 def log(log_type, log_level, message):
@@ -25,23 +26,28 @@ def is_usb(device):
         if usb.util.find_descriptor(cfg, bInterfaceClass = 8) != None:
             return True
 
-def init_handler():
-    context = pyudev.Context()
-    monitor = pyudev.Monitor.from_netlink(context)
+def monitoring():
+    print "Monitoring..."
+    monitor = pyudev.Monitor.from_netlink(pyudev.Context())
     monitor.filter_by(subsystem = "usb")
     for device in iter(monitor.poll, None):
-        uid = str(device.get("ID_VENDOR_ID")) + ':' + str(device.get("ID_MODEL_ID"))
+        vendor_id = str(device.get("ID_VENDOR_ID"))
+        model_id = str(device.get("ID_MODEL_ID"))
+        uid = vendor_id + ':' + model_id
         if device.action == "add":
             if device.get("ID_VENDOR_ID") != None:
-                dev = usb.core.find(find_all = False, idVendor = int(device.get("ID_VENDOR_ID"), 16), idProduct = int(device.get("ID_MODEL_ID"), 16), custom_match = is_usb)
+                dev = usb.core.find(find_all = False, idVendor = int(vendor_id, 16), idProduct = int(model_id, 16), custom_match = is_usb)
                 if dev != None:
                     if uid not in USB_LIST:
                         USB_LIST.append(uid)
                         log("connection", "INFO", uid + " is plugged in")
+                        print uid + " is plugged in."
         if device.action == "remove":
             if uid in USB_LIST:
                 USB_LIST.remove(uid)
                 log("connection", "INFO", uid + " is unplugged")
+                print uid + " is unplugged."
 
-create_log_folder("connection")
-init_handler()
+if __name__ == "__main__":
+    create_log_folder("connection")
+    threading.Thread(target = monitoring).start()
