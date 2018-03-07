3bf60858b357b1030683debbcff3c6666a44eb63
quanght55 <htq0505@gmail.com>
2018-03-07 11:40:23 +0700

    Add detect mount function

--- detectUSB.py
+++ detectUSB.py
@@ -1,4 +1,4 @@
-import re, sys, os, datetime, time
+import re, sys, os, datetime, time, getpass
 import subprocess
 import pyudev
 import usb.core, usb.util
@@ -6,7 +6,72 @@ import usb.core, usb.util
 LOG_FOLDER='/home/quanghuynh/USBanner/test/logs/{}'.format(time.strftime("%Y-%m-%d"))
 LOG_FORMAT='[0] [{0}] [{1}]: {2}'
 
-def event_handle():
+# Credit goes to MrVallentin, Dont know why after run setup.py I cannot use this lib
+# https://github.com/MrVallentin/mount.py
+def list_media_devices():
+	# If the major number is 8, that indicates it to be a disk device.
+	#
+	# The minor number is the partitions on the same device:
+	# - 0 means the entire disk
+	# - 1 is the primary
+	# - 2 is extended
+	# - 5 is logical partitions
+	# The maximum number of partitions is 15.
+	#
+	# Use `$ sudo fdisk -l` and `$ sudo sfdisk -l /dev/sda` for more information.
+	with open("/proc/partitions", "r") as f:
+		devices = []
+		
+		for line in f.readlines()[2:]: # skip header lines
+			words = [ word.strip() for word in line.split() ]
+			minor_number = int(words[1])
+			device_name = words[3]
+			
+			if (minor_number % 16) == 0:
+				path = "/sys/class/block/" + device_name
+				
+				if os.path.islink(path):
+					if os.path.realpath(path).find("/usb") > 0:
+						devices.append("/dev/" + device_name)
+		
+		return devices
+
+def get_device_name(device):
+	return os.path.basename(device)
+
+def get_device_block_path(device):
+	return "/sys/block/%s" % get_device_name(device)
+
+def get_media_path(device):
+	directory = "/media/"+getpass.getuser()+"/"
+	return directory+next(os.walk(directory))[1][0]
+
+def get_partition(device):
+	os.system("fdisk -l %s > output" % device)
+	with open("output", "r") as f:
+		data = f.read()
+		return data.split("\n")[-2].split()[0].strip()
+
+def is_mounted(device):
+	return os.path.ismount(get_media_path(device))
+
+def unmount_partition(name="usb"):
+	path = get_media_path(name)
+	if is_mounted(path):
+		os.system("umount " + path)
+		#os.system("rm -rf " + path)
+
+def mount(device, name=None):
+	if not name:
+		name = get_device_name(device)
+	mount_partition(get_partition(device), name)
+
+def unmount(device, name=None):
+	if not name:
+		name = get_device_name(device)
+	unmount_partition(name)
+
+def detect_connection():
 	logs_folder_creator()
 	context = pyudev.Context()
 	monitor = pyudev.Monitor.from_netlink(context)
@@ -42,6 +107,17 @@ def logs_folder_creator():
 	if os.path.exists("/home/quanghuynh/USBanner/test/logs/{}".format(time.strftime("%Y-%m-%d"))) == False:
 		os.makedirs("/home/quanghuynh/USBanner/test/logs/{}".format(time.strftime("%Y-%m-%d")))
 
-event_handle()
+def cls():
+	os.system("cls" if os.name == "nt" else "clear")
 
+def main():
+	for i in list_media_devices():
+		if is_mounted(i):
+			unmount(i)
+		else:
+			pass
+
+
+if __name__=="__main__":
+	main()
 
